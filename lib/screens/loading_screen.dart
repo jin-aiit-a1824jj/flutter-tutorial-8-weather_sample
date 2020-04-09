2da5fb38d2f0bc114d08f67d951f9c8ab16d1e11
a1824jj <a1824jj@aiit.ac.jp>
2020-04-09 12:07:33 +0900

    use api & get data - OpenWeatherMap API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weathersampel/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:weathersampel/services/networking.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,49 +10,44 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    this.latitude = location.latitude;
+    this.longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&appid=$apiKey');
+
+    var decodeData = await networkHelper.getData();
+
+    var longitude = decodeData['coord']['lon'];
+    print(longitude);
+
+    var weatherDescription = decodeData['weather'][0]['description'];
+    print(weatherDescription);
+
+    var temperature = decodeData['main']['temp'];
+    print(temperature);
+
+    var condition = decodeData['weather'][0]['id'];
+    print(condition);
+
+    var cityName = decodeData['name'];
+    print(cityName);
   }
 
-  void getData() async {
-    http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22');
-    if (response.statusCode == 200) {
-      String data = response.body;
-      print(data);
-
-      var decodeData = jsonDecode(data);
-
-      var longitude = decodeData['coord']['lon'];
-      print(longitude);
-
-      var weatherDescription = decodeData['weather'][0]['description'];
-      print(weatherDescription);
-
-      var temperature = decodeData['main']['temp'];
-      print(temperature);
-
-      var condition = decodeData['weather'][0]['id'];
-      print(condition);
-
-      var cityName = decodeData['name'];
-      print(cityName);
-
-    } else {
-      print(response.statusCode);
-    }
-  }
+  double latitude;
+  double longitude;
 }
+
+const apiKey = 'apikey';
